62390420e08380a0812d6a9210f8189633095506
Rafa Souza <rafaelsouza-nvg@hotmail.com>
2022-08-08 19:57:58 -0300

    Alterei o arquivo 03 da aula 26 'USE'

--- aula26-pdo/exemplo-03.php
+++ aula26-pdo/exemplo-03.php
@@ -2,7 +2,7 @@
 
 $conn = new PDO('mysql:dbname=dbphp7;host=localhost', 'root', '');
 
-$stmt = $conn->prepare("INSERT INTO tb_usuarios VAL(deslogin, dessenha) UES(:login, :password)");
+$stmt = $conn->prepare("INSERT INTO tb_usuarios VAL(deslogin, dessenha) USE(:login, :password)");
 
 $login = 'jose';
 $password = '1234567890';
